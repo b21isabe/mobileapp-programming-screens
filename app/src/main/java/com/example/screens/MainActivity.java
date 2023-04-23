d223f60c5a2a59cec5df44fb95b364256e242cf8
Isak Bergström <b21isabe@student.his.se>
2023-04-23 18:46:15 +0200

    Löst så att datan som skrivits i main activity visas i den andra activityn

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,12 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button clickbutton = findViewById(R.id.clickbutton);
+        final EditText age = findViewById(R.id.editTextAge);
+        final EditText name = findViewById(R.id.editTextName);
+
         clickbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, activity_second.class);
-                //intent.putExtra("name", "Daniel"); // Optional
-                //intent.putExtra("number", 1); // Optional
+                intent.putExtra("name", name.getText().toString()); // Optional
+                intent.putExtra("age",age.getText().toString()); // Optional
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/screens/activity_second.java
+++ app/src/main/java/com/example/screens/activity_second.java
@@ -2,7 +2,9 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class activity_second extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class activity_second extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        String name ="";
+        String age ="";
+
+        Intent intent = getIntent();
+        if (intent != null){
+            name = intent.getStringExtra("name");
+            age = intent.getStringExtra("age");
+        }
+        TextView TextViewName =findViewById(R.id.textViewName);
+        TextView TextViewAge =findViewById(R.id.textViewAge);
+
+        TextViewName.setText(name);
+        TextViewAge.setText(age);
+
+
     }
 }
